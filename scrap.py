3e83962c874837622dac326ffcb87eb130fb3b2b
sandeshrathod <44092192+sandeshrathod@users.noreply.github.com>
2019-06-11 21:18:32 +0530

    Update scrap.py

--- scrap.py
+++ scrap.py
@@ -1,4 +1,21 @@
 #sandesh rathod
+from flask import Flask , rendertemplate
+
+app = Flask(__name__)
+@app.route('\')
+
+def home():
+    return 'that was an ind3x of https:\\flaskhub.000webhostapp.com
+
+if __name__ == '__main__':
+      app.run(DEBUG = 1)
+
+
+
+
+
+
+
 
 def rec(x , y):
     p = x
